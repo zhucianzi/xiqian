c10aaacdc8211af5a6feb230d4a3f39d75bca6b1
zhucianzi <1696774136@qq.com>
2024-03-30 21:31:48 +0800

    新的unocss魔法

--- uno.config.ts
+++ uno.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig,presetUno } from "unocss";
+import { defineConfig, presetUno, presetAttributify } from "unocss";
 
 export default defineConfig({
     presets: [
-        presetUno()
+        presetUno(),
+        presetAttributify()
     ]
 });
